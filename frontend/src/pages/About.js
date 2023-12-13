a7c382246da55ba0e8a591f59bbd74c06f8bc45a
Fredy Pena <tredy@outlook.com>
2023-12-13 14:40:35 -0500

    Carga de proyecto FrontEnd Backend - siguiente paso => modificacion personalizada

---
+++ frontend/src/pages/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div style={{marginTop:"150px"}}>
+      <h2>{""}
+      This is React User Contact management System Application using Node And Express JS along with Routing
+      </h2>
+    </div>
+  );
+};
+
+export default About;
